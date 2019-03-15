5569ff0b24729510df648822a4810eab459585b0
Chris Southern <chris@blockthrough.co>
2019-03-15 18:17:15 -0400

    Errors

--- stackfinder/stackfinder.pb.go
+++ stackfinder/stackfinder.pb.go
@@ -155,30 +155,320 @@ func (m *HealthResponse) GetChromeProcesses() int32 {
 	return 0
 }
 
+type StackFinderSiteRequest struct {
+	Url                  string   `protobuf:"bytes,1,opt,name=Url,proto3" json:"Url,omitempty"`
+	IncludeUnknown       bool     `protobuf:"varint,2,opt,name=IncludeUnknown,proto3" json:"IncludeUnknown,omitempty"`
+	ScreenShot           bool     `protobuf:"varint,3,opt,name=ScreenShot,proto3" json:"ScreenShot,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *StackFinderSiteRequest) Reset()         { *m = StackFinderSiteRequest{} }
+func (m *StackFinderSiteRequest) String() string { return proto.CompactTextString(m) }
+func (*StackFinderSiteRequest) ProtoMessage()    {}
+func (*StackFinderSiteRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_3b833ff3c53c1a5e, []int{3}
+}
+
+func (m *StackFinderSiteRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_StackFinderSiteRequest.Unmarshal(m, b)
+}
+func (m *StackFinderSiteRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_StackFinderSiteRequest.Marshal(b, m, deterministic)
+}
+func (m *StackFinderSiteRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_StackFinderSiteRequest.Merge(m, src)
+}
+func (m *StackFinderSiteRequest) XXX_Size() int {
+	return xxx_messageInfo_StackFinderSiteRequest.Size(m)
+}
+func (m *StackFinderSiteRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_StackFinderSiteRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_StackFinderSiteRequest proto.InternalMessageInfo
+
+func (m *StackFinderSiteRequest) GetUrl() string {
+	if m != nil {
+		return m.Url
+	}
+	return ""
+}
+
+func (m *StackFinderSiteRequest) GetIncludeUnknown() bool {
+	if m != nil {
+		return m.IncludeUnknown
+	}
+	return false
+}
+
+func (m *StackFinderSiteRequest) GetScreenShot() bool {
+	if m != nil {
+		return m.ScreenShot
+	}
+	return false
+}
+
+type StackFinderHTMLRequest struct {
+	HTML                 string   `protobuf:"bytes,1,opt,name=HTML,proto3" json:"HTML,omitempty"`
+	Width                int64    `protobuf:"varint,2,opt,name=Width,proto3" json:"Width,omitempty"`
+	Height               int64    `protobuf:"varint,3,opt,name=Height,proto3" json:"Height,omitempty"`
+	IncludeUnknown       bool     `protobuf:"varint,4,opt,name=IncludeUnknown,proto3" json:"IncludeUnknown,omitempty"`
+	ScreenShot           bool     `protobuf:"varint,5,opt,name=ScreenShot,proto3" json:"ScreenShot,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *StackFinderHTMLRequest) Reset()         { *m = StackFinderHTMLRequest{} }
+func (m *StackFinderHTMLRequest) String() string { return proto.CompactTextString(m) }
+func (*StackFinderHTMLRequest) ProtoMessage()    {}
+func (*StackFinderHTMLRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_3b833ff3c53c1a5e, []int{4}
+}
+
+func (m *StackFinderHTMLRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_StackFinderHTMLRequest.Unmarshal(m, b)
+}
+func (m *StackFinderHTMLRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_StackFinderHTMLRequest.Marshal(b, m, deterministic)
+}
+func (m *StackFinderHTMLRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_StackFinderHTMLRequest.Merge(m, src)
+}
+func (m *StackFinderHTMLRequest) XXX_Size() int {
+	return xxx_messageInfo_StackFinderHTMLRequest.Size(m)
+}
+func (m *StackFinderHTMLRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_StackFinderHTMLRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_StackFinderHTMLRequest proto.InternalMessageInfo
+
+func (m *StackFinderHTMLRequest) GetHTML() string {
+	if m != nil {
+		return m.HTML
+	}
+	return ""
+}
+
+func (m *StackFinderHTMLRequest) GetWidth() int64 {
+	if m != nil {
+		return m.Width
+	}
+	return 0
+}
+
+func (m *StackFinderHTMLRequest) GetHeight() int64 {
+	if m != nil {
+		return m.Height
+	}
+	return 0
+}
+
+func (m *StackFinderHTMLRequest) GetIncludeUnknown() bool {
+	if m != nil {
+		return m.IncludeUnknown
+	}
+	return false
+}
+
+func (m *StackFinderHTMLRequest) GetScreenShot() bool {
+	if m != nil {
+		return m.ScreenShot
+	}
+	return false
+}
+
+type StackFinderResponse struct {
+	Count                int32                       `protobuf:"varint,1,opt,name=Count,proto3" json:"Count,omitempty"`
+	Items                []*StackFinderResponse_Item `protobuf:"bytes,2,rep,name=items,proto3" json:"items,omitempty"`
+	Screenshot           []byte                      `protobuf:"bytes,3,opt,name=Screenshot,proto3" json:"Screenshot,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}                    `json:"-"`
+	XXX_unrecognized     []byte                      `json:"-"`
+	XXX_sizecache        int32                       `json:"-"`
+}
+
+func (m *StackFinderResponse) Reset()         { *m = StackFinderResponse{} }
+func (m *StackFinderResponse) String() string { return proto.CompactTextString(m) }
+func (*StackFinderResponse) ProtoMessage()    {}
+func (*StackFinderResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_3b833ff3c53c1a5e, []int{5}
+}
+
+func (m *StackFinderResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_StackFinderResponse.Unmarshal(m, b)
+}
+func (m *StackFinderResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_StackFinderResponse.Marshal(b, m, deterministic)
+}
+func (m *StackFinderResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_StackFinderResponse.Merge(m, src)
+}
+func (m *StackFinderResponse) XXX_Size() int {
+	return xxx_messageInfo_StackFinderResponse.Size(m)
+}
+func (m *StackFinderResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_StackFinderResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_StackFinderResponse proto.InternalMessageInfo
+
+func (m *StackFinderResponse) GetCount() int32 {
+	if m != nil {
+		return m.Count
+	}
+	return 0
+}
+
+func (m *StackFinderResponse) GetItems() []*StackFinderResponse_Item {
+	if m != nil {
+		return m.Items
+	}
+	return nil
+}
+
+func (m *StackFinderResponse) GetScreenshot() []byte {
+	if m != nil {
+		return m.Screenshot
+	}
+	return nil
+}
+
+type StackFinderResponse_Item struct {
+	ItemId               int32       `protobuf:"varint,1,opt,name=ItemId,proto3" json:"ItemId,omitempty"`
+	ItemName             string      `protobuf:"bytes,2,opt,name=ItemName,proto3" json:"ItemName,omitempty"`
+	Url                  string      `protobuf:"bytes,3,opt,name=Url,proto3" json:"Url,omitempty"`
+	ItemCategory         []*Category `protobuf:"bytes,4,rep,name=ItemCategory,proto3" json:"ItemCategory,omitempty"`
+	InitiatorItemId      int32       `protobuf:"varint,5,opt,name=InitiatorItemId,proto3" json:"InitiatorItemId,omitempty"`
+	InitiatorItemName    string      `protobuf:"bytes,6,opt,name=InitiatorItemName,proto3" json:"InitiatorItemName,omitempty"`
+	InitiatorCategory    []*Category `protobuf:"bytes,7,rep,name=InitiatorCategory,proto3" json:"InitiatorCategory,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}    `json:"-"`
+	XXX_unrecognized     []byte      `json:"-"`
+	XXX_sizecache        int32       `json:"-"`
+}
+
+func (m *StackFinderResponse_Item) Reset()         { *m = StackFinderResponse_Item{} }
+func (m *StackFinderResponse_Item) String() string { return proto.CompactTextString(m) }
+func (*StackFinderResponse_Item) ProtoMessage()    {}
+func (*StackFinderResponse_Item) Descriptor() ([]byte, []int) {
+	return fileDescriptor_3b833ff3c53c1a5e, []int{5, 0}
+}
+
+func (m *StackFinderResponse_Item) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_StackFinderResponse_Item.Unmarshal(m, b)
+}
+func (m *StackFinderResponse_Item) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_StackFinderResponse_Item.Marshal(b, m, deterministic)
+}
+func (m *StackFinderResponse_Item) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_StackFinderResponse_Item.Merge(m, src)
+}
+func (m *StackFinderResponse_Item) XXX_Size() int {
+	return xxx_messageInfo_StackFinderResponse_Item.Size(m)
+}
+func (m *StackFinderResponse_Item) XXX_DiscardUnknown() {
+	xxx_messageInfo_StackFinderResponse_Item.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_StackFinderResponse_Item proto.InternalMessageInfo
+
+func (m *StackFinderResponse_Item) GetItemId() int32 {
+	if m != nil {
+		return m.ItemId
+	}
+	return 0
+}
+
+func (m *StackFinderResponse_Item) GetItemName() string {
+	if m != nil {
+		return m.ItemName
+	}
+	return ""
+}
+
+func (m *StackFinderResponse_Item) GetUrl() string {
+	if m != nil {
+		return m.Url
+	}
+	return ""
+}
+
+func (m *StackFinderResponse_Item) GetItemCategory() []*Category {
+	if m != nil {
+		return m.ItemCategory
+	}
+	return nil
+}
+
+func (m *StackFinderResponse_Item) GetInitiatorItemId() int32 {
+	if m != nil {
+		return m.InitiatorItemId
+	}
+	return 0
+}
+
+func (m *StackFinderResponse_Item) GetInitiatorItemName() string {
+	if m != nil {
+		return m.InitiatorItemName
+	}
+	return ""
+}
+
+func (m *StackFinderResponse_Item) GetInitiatorCategory() []*Category {
+	if m != nil {
+		return m.InitiatorCategory
+	}
+	return nil
+}
+
 func init() {
 	proto.RegisterType((*HealthRequest)(nil), "stackfinder_service.HealthRequest")
 	proto.RegisterType((*Category)(nil), "stackfinder_service.Category")
 	proto.RegisterType((*HealthResponse)(nil), "stackfinder_service.HealthResponse")
+	proto.RegisterType((*StackFinderSiteRequest)(nil), "stackfinder_service.StackFinderSiteRequest")
+	proto.RegisterType((*StackFinderHTMLRequest)(nil), "stackfinder_service.StackFinderHTMLRequest")
+	proto.RegisterType((*StackFinderResponse)(nil), "stackfinder_service.StackFinderResponse")
+	proto.RegisterType((*StackFinderResponse_Item)(nil), "stackfinder_service.StackFinderResponse.Item")
 }
 
 func init() { proto.RegisterFile("stackfinder.proto", fileDescriptor_3b833ff3c53c1a5e) }
 
 var fileDescriptor_3b833ff3c53c1a5e = []byte{
-	// 216 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x7c, 0x50, 0xc1, 0x4a, 0xc5, 0x30,
-	0x10, 0xa4, 0x51, 0xab, 0x6f, 0xc1, 0xf7, 0x70, 0x05, 0x09, 0x9e, 0x1e, 0xf1, 0xd2, 0x53, 0x0e,
-	0xfa, 0x09, 0x0f, 0xc4, 0x5e, 0x44, 0x53, 0xd0, 0xa3, 0xc4, 0x76, 0xb5, 0xc5, 0xb6, 0xa9, 0xd9,
-	0x54, 0xf0, 0xef, 0x85, 0xb4, 0x05, 0x15, 0xf1, 0xb6, 0x33, 0xec, 0xce, 0xec, 0x0c, 0x9c, 0x70,
-	0xb0, 0xe5, 0xdb, 0x4b, 0xd3, 0x57, 0xe4, 0xf5, 0xe0, 0x5d, 0x70, 0x78, 0xfa, 0x8d, 0x7a, 0x62,
-	0xf2, 0x1f, 0x4d, 0x49, 0x6a, 0x03, 0xc7, 0x37, 0x64, 0xdb, 0x50, 0x1b, 0x7a, 0x1f, 0x89, 0x83,
-	0xd2, 0x70, 0xb4, 0xb3, 0x81, 0x5e, 0x9d, 0xff, 0xc4, 0x35, 0x88, 0xbc, 0x92, 0xc9, 0x36, 0xc9,
-	0x0e, 0x8c, 0xc8, 0x2b, 0x44, 0xd8, 0xbf, 0xb5, 0x1d, 0x49, 0xb1, 0x4d, 0xb2, 0x95, 0x89, 0xb3,
-	0x6a, 0x61, 0xbd, 0x08, 0xf0, 0xe0, 0x7a, 0x26, 0x3c, 0x83, 0xb4, 0x08, 0x36, 0x8c, 0x1c, 0x2f,
-	0x57, 0x66, 0x46, 0x28, 0xe1, 0xf0, 0x81, 0x3c, 0x37, 0xae, 0x9f, 0x05, 0x16, 0x88, 0x19, 0x6c,
-	0x76, 0xb5, 0x77, 0x1d, 0xdd, 0x79, 0x57, 0x12, 0x33, 0xb1, 0xdc, 0x8b, 0xa6, 0xbf, 0xe9, 0xcb,
-	0x47, 0x10, 0xc5, 0x35, 0xde, 0x43, 0x3a, 0x79, 0xa2, 0xd2, 0x7f, 0x84, 0xd2, 0x3f, 0x12, 0x9d,
-	0x5f, 0xfc, 0xbb, 0x33, 0x3d, 0xfd, 0x9c, 0xc6, 0x8e, 0xae, 0xbe, 0x02, 0x00, 0x00, 0xff, 0xff,
-	0x0c, 0xdf, 0x1f, 0x76, 0x38, 0x01, 0x00, 0x00,
+	// 500 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x54, 0xcd, 0x6e, 0xd3, 0x40,
+	0x10, 0x56, 0xec, 0x38, 0x4d, 0x87, 0x92, 0xd0, 0x69, 0x55, 0x59, 0x91, 0x40, 0x91, 0x91, 0x50,
+	0x24, 0xc0, 0x87, 0xf2, 0x04, 0x28, 0x52, 0x15, 0x8b, 0x1f, 0xc1, 0x9a, 0xc2, 0x11, 0x19, 0x7b,
+	0xa8, 0xad, 0x26, 0xde, 0xb2, 0xbb, 0x01, 0xf1, 0x3a, 0x5c, 0xb9, 0xf2, 0x0e, 0xbc, 0x16, 0xda,
+	0xf5, 0x3a, 0x75, 0x1c, 0x8b, 0xe6, 0x94, 0xfd, 0xc6, 0x33, 0xf3, 0x7d, 0xbb, 0xf3, 0x4d, 0xe0,
+	0x58, 0xaa, 0x24, 0xbd, 0xfe, 0x5a, 0x94, 0x19, 0x89, 0xf0, 0x46, 0x70, 0xc5, 0xf1, 0xa4, 0x11,
+	0xfa, 0x2c, 0x49, 0x7c, 0x2f, 0x52, 0x0a, 0xc6, 0x70, 0x7f, 0x41, 0xc9, 0x52, 0xe5, 0x8c, 0xbe,
+	0xad, 0x49, 0xaa, 0x20, 0x84, 0xe1, 0x3c, 0x51, 0x74, 0xc5, 0xc5, 0x4f, 0x1c, 0x81, 0x13, 0x65,
+	0x7e, 0x6f, 0xda, 0x9b, 0x79, 0xcc, 0x89, 0x32, 0x44, 0xe8, 0xbf, 0x4d, 0x56, 0xe4, 0x3b, 0xd3,
+	0xde, 0xec, 0x90, 0x99, 0x73, 0xb0, 0x84, 0x51, 0xdd, 0x40, 0xde, 0xf0, 0x52, 0x12, 0x9e, 0xc1,
+	0x20, 0x56, 0x89, 0x5a, 0x4b, 0x53, 0x79, 0xc8, 0x2c, 0x42, 0x1f, 0x0e, 0x3e, 0x92, 0x90, 0x05,
+	0x2f, 0x6d, 0x83, 0x1a, 0xe2, 0x0c, 0xc6, 0xf3, 0x5c, 0xf0, 0x15, 0xbd, 0x13, 0x3c, 0x25, 0x29,
+	0x49, 0xfa, 0xae, 0x21, 0x6d, 0x87, 0x03, 0x01, 0x67, 0xb1, 0xbe, 0xc5, 0x85, 0xb9, 0x45, 0x5c,
+	0x28, 0xb2, 0xba, 0xf1, 0x01, 0xb8, 0x97, 0x62, 0x69, 0x29, 0xf5, 0x11, 0x9f, 0xc0, 0x28, 0x2a,
+	0xd3, 0xe5, 0x3a, 0xa3, 0xcb, 0xf2, 0xba, 0xe4, 0x3f, 0x2a, 0xda, 0x21, 0x6b, 0x45, 0xf1, 0x11,
+	0x40, 0x9c, 0x0a, 0xa2, 0x32, 0xce, 0xb9, 0x32, 0xc4, 0x43, 0xd6, 0x88, 0x04, 0xbf, 0x7a, 0x5b,
+	0xa4, 0x8b, 0x0f, 0x6f, 0x5e, 0xd7, 0xa4, 0x08, 0x7d, 0x0d, 0x2d, 0xab, 0x39, 0xe3, 0x29, 0x78,
+	0x9f, 0x8a, 0x4c, 0xe5, 0x86, 0xcd, 0x65, 0x15, 0xd0, 0x8f, 0xb2, 0xa0, 0xe2, 0x2a, 0xaf, 0x08,
+	0x5c, 0x66, 0x51, 0x87, 0xc8, 0xfe, 0x1e, 0x22, 0xbd, 0x1d, 0x91, 0x7f, 0x5d, 0x38, 0x69, 0x88,
+	0xdc, 0x0c, 0xe3, 0x14, 0xbc, 0x39, 0x5f, 0x97, 0xca, 0x4e, 0xb1, 0x02, 0x38, 0x07, 0xaf, 0x50,
+	0xb4, 0x92, 0xbe, 0x33, 0x75, 0x67, 0xf7, 0xce, 0x9f, 0x87, 0x1d, 0xd6, 0x08, 0x3b, 0xda, 0x85,
+	0x91, 0xa2, 0x15, 0xab, 0x6a, 0x6f, 0x25, 0xc9, 0xfa, 0xdd, 0x8e, 0x58, 0x23, 0x32, 0xf9, 0xe3,
+	0x40, 0x5f, 0xe7, 0xeb, 0xbb, 0xeb, 0xdf, 0x8d, 0x95, 0x2c, 0xc2, 0x09, 0x0c, 0xf5, 0xa9, 0x61,
+	0xa9, 0x0d, 0xae, 0xc7, 0xe9, 0xde, 0x8e, 0xf3, 0x25, 0x1c, 0xe9, 0xaf, 0xb5, 0x39, 0xfd, 0xbe,
+	0x91, 0xfe, 0xb0, 0x53, 0x7a, 0x9d, 0xc4, 0xb6, 0x4a, 0xb4, 0xcf, 0xa2, 0xb2, 0x50, 0x45, 0xa2,
+	0xb8, 0xb0, 0x8a, 0xbc, 0xca, 0x67, 0xad, 0x30, 0x3e, 0x83, 0xe3, 0xad, 0x90, 0xd1, 0x38, 0x30,
+	0x62, 0x76, 0x3f, 0xe0, 0xab, 0x46, 0xf6, 0x46, 0xdf, 0xc1, 0x3e, 0xfa, 0x76, 0xeb, 0xce, 0x7f,
+	0x3b, 0xe0, 0xc4, 0x17, 0xf8, 0x5e, 0x1b, 0x46, 0xef, 0x15, 0x06, 0x9d, 0x2d, 0xb6, 0xb6, 0x76,
+	0xf2, 0xf8, 0xbf, 0x39, 0xd6, 0x0b, 0x39, 0x8c, 0x5b, 0xcb, 0x83, 0x4f, 0xef, 0x9a, 0x7c, 0x63,
+	0xc5, 0x26, 0xb3, 0x7d, 0x6d, 0xd2, 0x62, 0x32, 0x6b, 0x71, 0x27, 0x53, 0x63, 0xaf, 0xf6, 0x67,
+	0xfa, 0x32, 0x30, 0xff, 0x6d, 0x2f, 0xfe, 0x05, 0x00, 0x00, 0xff, 0xff, 0x9e, 0x45, 0xd5, 0xf7,
+	0xf0, 0x04, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -194,6 +484,8 @@ const _ = grpc.SupportPackageIsVersion4
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type SFClient interface {
 	Health(ctx context.Context, in *HealthRequest, opts ...grpc.CallOption) (*HealthResponse, error)
+	StackFinderSite(ctx context.Context, in *StackFinderSiteRequest, opts ...grpc.CallOption) (*StackFinderResponse, error)
+	StackFinderHTML(ctx context.Context, in *StackFinderHTMLRequest, opts ...grpc.CallOption) (*StackFinderResponse, error)
 }
 
 type sFClient struct {
@@ -213,9 +505,29 @@ func (c *sFClient) Health(ctx context.Context, in *HealthRequest, opts ...grpc.C
 	return out, nil
 }
 
+func (c *sFClient) StackFinderSite(ctx context.Context, in *StackFinderSiteRequest, opts ...grpc.CallOption) (*StackFinderResponse, error) {
+	out := new(StackFinderResponse)
+	err := c.cc.Invoke(ctx, "/stackfinder_service.SF/StackFinderSite", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *sFClient) StackFinderHTML(ctx context.Context, in *StackFinderHTMLRequest, opts ...grpc.CallOption) (*StackFinderResponse, error) {
+	out := new(StackFinderResponse)
+	err := c.cc.Invoke(ctx, "/stackfinder_service.SF/StackFinderHTML", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // SFServer is the server API for SF service.
 type SFServer interface {
 	Health(context.Context, *HealthRequest) (*HealthResponse, error)
+	StackFinderSite(context.Context, *StackFinderSiteRequest) (*StackFinderResponse, error)
+	StackFinderHTML(context.Context, *StackFinderHTMLRequest) (*StackFinderResponse, error)
 }
 
 func RegisterSFServer(s *grpc.Server, srv SFServer) {
@@ -240,6 +552,42 @@ func _SF_Health_Handler(srv interface{}, ctx context.Context, dec func(interface
 	return interceptor(ctx, in, info, handler)
 }
 
+func _SF_StackFinderSite_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(StackFinderSiteRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(SFServer).StackFinderSite(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/stackfinder_service.SF/StackFinderSite",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(SFServer).StackFinderSite(ctx, req.(*StackFinderSiteRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _SF_StackFinderHTML_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(StackFinderHTMLRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(SFServer).StackFinderHTML(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/stackfinder_service.SF/StackFinderHTML",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(SFServer).StackFinderHTML(ctx, req.(*StackFinderHTMLRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _SF_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "stackfinder_service.SF",
 	HandlerType: (*SFServer)(nil),
@@ -247,6 +595,14 @@ var _SF_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Health",
 			Handler:    _SF_Health_Handler,
+		},
+		{
+			MethodName: "StackFinderSite",
+			Handler:    _SF_StackFinderSite_Handler,
+		},
+		{
+			MethodName: "StackFinderHTML",
+			Handler:    _SF_StackFinderHTML_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
